b6d4b9fc26b2ecb3016387d789d0352c4f409390
naorzr <naorzruk@gmail.com>
2022-08-31 09:02:47 +0300

    a

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import logo from './logo.svg';
+import { LivecycleSdk } from 'livecycle';
 import './App.css';
 
 function App() {
+  LivecycleSdk.init();
   return (
     <div className="App">
       <header className="App-header">
